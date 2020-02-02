beea65745150b695c571cd77f00c45ce8cbd7a6d
MohammadRidha mousavi <mosavi.smr@gmail.com>
2020-02-02 13:50:08 +0330

    creating auth&panel&register&login page with Routing

--- MadPay724-Admin/src/app/app.module.ts
+++ MadPay724-Admin/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+
+import { RouterModule } from '@angular/router';
+import { adminRoutes } from './routes/routes';
+import { AuthModule } from './auth/auth.module';
+import { PanelModule } from './panel/panel.module';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      AuthModule,
+      PanelModule,
+      RouterModule.forRoot(adminRoutes)
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
